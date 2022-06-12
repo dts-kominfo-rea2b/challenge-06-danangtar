2b623b7e72161be5e762555b1e3c209df2e88712
Danang Adi Nugroho <danangtar@gmail.com>
2022-06-12 22:00:53 +0700

    add read files callbacks

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,48 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  readFile((firstWord) => {
+    readNextFile(firstWord, (secondWord) => {
+      readLastFile(secondWord, (arr) => {
+        fnCallback(null, arr);
+      });
+    })
+  });
+};
+
+function readFile(nextFile) {
+  fs.readFile(file1, (err, data) => {
+    if (err) throw err
+    var data = JSON.parse(data);
+    var word = [];
+    word.push(lastWord(data.message));
+    nextFile(word);
+  });
+}
+
+function readNextFile(word, nextFile) {
+  fs.readFile(file2, (err, data) => {
+    if (err) throw err
+    var data = JSON.parse(data);
+    word.push(lastWord(data[0].message))
+    nextFile(word);
+  });
+}
+
+function readLastFile(word, nextFile) {
+  fs.readFile(file3, (err, data) => {
+    if (err) throw err
+    var data = JSON.parse(data);
+    word.push(lastWord(data[0].data.message));
+    nextFile(word);
+  });
+}
+
+function lastWord(words) {
+  var n = words.split(" ");
+  return n[n.length - 1];
+}
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
